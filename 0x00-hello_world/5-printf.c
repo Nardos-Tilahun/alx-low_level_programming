b89124bc1f71d082ee5c748d5748b0da53dfd2be
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-06 09:49:50 -0700

    hello print f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ *main - entry point where the program start
+ *
+ *Return: zero function
+ */
+int main(void)
+{
+	char *strng = "with proper grammar, but the outcome is a piece  of art,";
+	printf("%s", strng);
+	return(0);
+}
