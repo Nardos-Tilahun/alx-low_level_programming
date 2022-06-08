dd85192f0aa2a8485ad9ac4777ade698216a287a
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 12:09:56 -0700

    yes

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/*
+ *print_sign - it shows the sign of the number
+ *
+ *@n: int number of character
+ *
+ *Return: 1 if positive, 0 if zero, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return(1);
+	}
+	_putchar(43);
+	if else (n == 0)
+	{
+		return(0);
+	}
+	_putchar('0');
+	else (n < 0)
+	{
+		return(-1);
+	}
+	_putchar('-');
+	_putchar('\n');
+}
