6ff237f703ab844541efca03dc7f9f594d2b7b80
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 15:26:42 -0700

    yes

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,15 +14,15 @@ void times_table(void)
 	int j;
 	int m;
 
-	for (i = 48; i =< 57; i++)
+	for (i = 48; i <= 57; i++)
 	{
-		for (j = 48; j =< 57; j++)	
+		for (j = 48; j <= 57; j++)	
 		{
 			m = j * i; 
 
 			_putchar(m);
-			_putchar('44');
-			_putchar('32');	
+			_putchar(44);
+			_putchar(32);	
 		}
 		_putchar(10);
 	}
