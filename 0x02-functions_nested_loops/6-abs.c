257a2a8006312b4502bde8ffd50cffc52c64a5c5
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 12:41:40 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_abs : absolute value of an int
+ *
+ *@int is integer
+ *
+ *Return: integer value
+ */
+int _abs(int)
+{
+	int a;
+	int b;
+
+	if (a > 0 && a == 0)
+	{
+		b = a;
+		print('b');
+	}
+	else (a < 0)
+	{
+		b = a * -1;
+		putchar('b');
+
+	}
+	_putchar('\n');
+}
