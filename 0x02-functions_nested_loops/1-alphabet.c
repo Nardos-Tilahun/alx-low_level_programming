0ad9502b9c3902b41f2bdffbd70c96f76f958de4
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 07:53:28 -0700

    alphabet small

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- *main - 
+ *main - we start by printing small alphabet from a to z
  *
  *
- *
- *
+ *Return: 0
  */
 
 void print_alphabet(void)
 {
 	int a;
 
-	for(a = 97, a <= 122, a++)
+	for (a = 97, a <= 122, a++)
 	{
 		_putchar(a);
 	}
