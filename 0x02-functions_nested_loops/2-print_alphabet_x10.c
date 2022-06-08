2222f01bc6a97dc07747b896dce00648c40a3960
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 09:12:57 -0700

    check

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,19 @@
 
 void print_alphabet_x10(void)
 {
-	int n;
-	int m;
+        int n;
+        int m;
 
-	for (n = 0; n >= 9; n++)
-	{
+        for (n = 0; n >= 9; n++)
+        {
+                for (m = 97; m >= 122; m++)
+                {
+                        _putchar(m);
+                }
+        _putchar(10);
+        }
+}
+
 		for (m = 97; m >= 122; m++)
 		{
 			_putchar(m);
