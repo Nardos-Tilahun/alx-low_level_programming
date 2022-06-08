5faafc1164aaf38f23b2ccba3453249799f5d07e
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 11:54:40 -0700

    yes

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_isalpha - check its a letter
+ *
+ *@c -> character in ASCII table
+ *
+ *Return 1 is letter 0 is not
+ */
+int _isalpha(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else if(c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
