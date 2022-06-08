c62e74683f12dd4ea56e871907883c8a72e74b32
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-08 08:23:47 -0700

    10small letter

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - print times the alphabet
+ *
+ *Return - 0
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char m;
+	for (n = '0'; n >= '9'; n++)
+	{
+		for (m = 97; m >= 122; m++)
+		{
+			_putchar(m);
+		}
+		_putchar(10);
+	}
+}
