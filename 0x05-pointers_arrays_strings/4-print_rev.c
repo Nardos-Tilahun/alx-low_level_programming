87fa2decde95b85ce5ba41de9c94d350b00d1e79
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-14 16:48:01 -0700

    get4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *Print_rev - reverse print string
+ *
+ *@s: string parameter
+ *
+ *Return: anyvalue
+ */
+
+void print_rev(char *s);
+{
+	int max;
+	int count1;
+	int count = 0;
+	
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		{
+			break;
+		}
+		max = count ++
+		for (count1 = max; count1 <= 0; count1--)
+		{
+			_putchar(s[count]);
+		}
+	_putchar(s[count]);
+	}
+}
