5d0fecbb4b10c705db3ca3708a9c036d0e1d9edf
Nardos-Tilahun <contactnardos@gmail.com>
2022-06-21 06:38:36 -0700

    get 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 include "main.h"
 /**
  *_memset - fills memory with byte
- *@*s: memory area
+ *@s: memory area
  *@b: constant byte
  *@n: bytes filled
  *Return: the pointer
@@ -9,8 +9,9 @@ include "main.h"
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
-		for (i = 0; i < n; i++);
-			*(s+i) = b;
+
+		for (i = 0; i < n; i++)
+			*(s + i) = b;
 
 	return (s);
 }
