d82cbf62adc3f88891d6b253e7dbce51ab97ef3a
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-11 10:13:04 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 97;
+	while (c < 122)
+	{
+		putchar(c);
+		c++;
+	}
+	c = 65;
+	while (c < 65)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
