9b24523799cc71e6e42307fcb5e78819c4e84b62
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-15 06:11:59 -0700

    7-last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,15 @@ int print_last_digit(int n)
 {
 	int a;
 
-	if (n < 0)
-	{
-		n = n * -1;
-	}
 	a = n % 10;
-	_putchar(a + '0');
-	return (a);
+	if (a < 0)
+	{
+		_putchar('0' - a);
+		return (-1 * a);
+	}
+	else
+	{
+		_putchar(a + '0');
+		return (a);
+	}
 }
