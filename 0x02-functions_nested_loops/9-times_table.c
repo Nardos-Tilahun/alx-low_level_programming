96ed3457866b94434dea9a52caab23189a23734e
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-15 06:47:13 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table -  prints the 9 times table, starting with 0
+ */
+void times_table(void)
+{
+	int n, x, y, p;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			for (y = 0; y < 10; y++)
+			{
+				p = x * y;
+				if (p < 10)
+				{
+					_putchar(' ');
+					_putchar(p + '0');
+				}
+				else if (p >= 10)
+				{
+					_putchar(p / 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+				if (y != 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
