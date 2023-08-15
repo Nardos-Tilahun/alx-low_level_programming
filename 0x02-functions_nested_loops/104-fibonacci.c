883f869429d17c2acd44e920f0c0e3e7fcf9fd46
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-15 14:37:26 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,41 @@
  */
 int main(void)
 {
-	unsigned long c, n = 2, m = 3;
-	int a = 2;
+	unsigned long c, n = 2, m = 3, a = 2, nh1, mh1, nh2, mh2, h1, h2;
 
 	printf("1, 2, ");
-	while (a < 98)
+	while (a < 97)
 	{
-		printf("%lu", m);
+		if (a < 92)
+		{
+			printf("%lu", m);
+			c =  m;
+			m = m + n;
+			n = c;
+		}
+		if (a > 93)
+		{
+			nh1 = n / 10000000000;
+			nh2 = n % 10000000000;
+			mh1 = m / 10000000000;
+			mh2 = m % 10000000000;
+			h1 = nh1 + mh1;
+			h2 = nh2 + mh2;
+			if (h2 > 9999999999)
+			{
+				h1 = nh1 + 1;
+				h2 = h2 % 10000000000;
+			}
+			printf("%lu%lu", h1, h2);
+			nh1 = mh1;
+			nh2 = mh2;
+			mh1 = h1;
+			mh2 = h2;
+		}
 		if (a != 97)
 		{
 			printf(", ");
 		}
-		c = m;
-		m = n + m;
-		n = c;
 		a++;
 	}
 	printf("\n");
