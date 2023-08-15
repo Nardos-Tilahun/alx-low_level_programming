00230bd9c2cce7d301607b5e01130abfd7456f05
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-15 13:02:08 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main -  Write a program that prints the first 50 Fibonacci numbers
+ * Return: 0 Always (Success)
+ */
+int main(void)
+{
+	long sum = 2, c, n = 2, m = 3;
+
+	printf("1, 2, ");
+	while (m < 4000000)
+	{
+		if (m % 2 == 0)
+		{
+			sum = sum + m;
+		}
+		c = m;
+		m = n + m;
+		n = c;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
