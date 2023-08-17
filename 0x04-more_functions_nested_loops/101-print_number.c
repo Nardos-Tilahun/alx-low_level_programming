0d8faea278302ab96038ed3d968c9e4a7b7e1f94
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-17 13:42:13 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,29 +6,16 @@
  */
 void print_number(int n)
 {
-	int m = 1, i;
+	unsigned int k = n;
 
-	if (n == 0)
+	if (n < 0)
 	{
-		_putchar(n + '0');
+		n *= -1;
+		k = n;
+		_putchar('-');
 	}
-	else
-	{
-		if (n < 0)
-		{
-			n = n * -1;
-			_putchar('-');
-		}
-		while (n >= m)
-		{
-			m = m * 10;
-		}
-		while (m != 1)
-		{
-			m = m / 10;
-			i = n / m;
-			n = n % m;
-			_putchar(i + '0');
-		}
-	}
+	k /= 10;
+	if (k != 0)
+		print_number(k);
+	_putchar((unsigned int)n % 10 + '0');
 }
