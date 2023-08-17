385d777f2e1704627b5984d7950cbaf5880797e7
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-16 22:19:51 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to be written
+ *
+ * Return: The character written
+ */
+char _putchar(char c) /* _putchar used to return c*/
+{
+	return (putchar(c));
+}
