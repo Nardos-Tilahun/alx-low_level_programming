6964b2e3c2be47ca57c48c6a508a208365457613
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-21 10:02:05 -0700

    6-puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints a string, in reverse followed by a new line, to stdout.
+ * @s: character pointer that accept string
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (((*(s + i) - '0') % 2) == 0)
+		{
+			_putchar(*(s + i));
+		}
+		i++;
+	}
+	_putchar('\n');
+}
