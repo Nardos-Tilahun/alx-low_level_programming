6be08cf6e85a930bf4e6a39464ed1d0e5ea0709f
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-21 06:01:11 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts -  prints a string, followed by a new line, to stdout.
+ * @str: character pointer that accept string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i++));
+	}
+}
