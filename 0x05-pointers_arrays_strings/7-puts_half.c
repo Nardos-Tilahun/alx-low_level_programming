f5bcabc43ae5a9a5d2b54b77c3156bc11df9a304
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-21 11:01:06 -0700

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,13 +6,21 @@
  */
 void puts_half(char *s)
 {
-	int i = 0;
+	int n, i = 0;
 
 	while (*(s + i) != '\0')
 	{
 		i++;
 	}
-	while (--i >= 0)
+	if (i % 2 == 0)
+	{
+		n = i / 2;
+	}
+	else
+	{
+		n = i / 2 - 1;
+	}
+	for (i = n ; *(s + i) != '\0'; i++)
 	{
 		_putchar(*(s + i));
 	}
