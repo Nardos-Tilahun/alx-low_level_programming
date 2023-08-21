a77329f96a7625b9694d11e9dec9d7c78f888bab
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-21 15:37:21 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+* main - check the code
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+	int pas[100];
+	int i, s, n;
+
+	s = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 100; i++)
+	{
+		pas[i] = rand() % 78;
+		s += (pas[i] + '0');
+		putchar(pas[i] + '0');
+		if ((2772 - s) - '0' < 78)
+		{
+			n = 2772 - s - '0';
+			s += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
+}
