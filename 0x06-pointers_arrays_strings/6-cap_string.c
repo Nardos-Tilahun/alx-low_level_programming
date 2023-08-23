37e245bc9b9e30aac31ef00acab0e832db8b9699
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-23 09:45:52 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -29,19 +29,19 @@ char *cap_string(char *s)
 		}
 		else if (s[i] == 
 		/*/switch (s[i])
-		case ' ':
-		case '\n':
-		case '\t':
-		case ',':
-		case ';':
-		case '.':
-		case '!':
-		case '?':
-		case '\"':
-		case '(':
-		case ')':
-		case '{':
-		case '}':
+		case 32:
+		case 10:
+		case 9:
+		case 44:
+		case 59:
+		case 46:
+		case 33:
+		case 63:
+		case 34:
+		case 40:
+		case 41:
+		case 123:
+		case 125:
 			if (s[i + 1] >= 97 && s[i + 1] <= 122)
 				s[i + 1] -= 32;
 	}
