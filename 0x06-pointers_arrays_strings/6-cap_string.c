98e851623b1ba3e4df8d4743fd20cdf8ccd97e74
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-23 04:56:05 -0700

    6-cap_string.c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - concatinate two string
+ * @s: second character pointer parameter
+ * Return: pointer character
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		switch (s[i])
+		case ' ':
+		case '\t':
+		case '\n':
+		case ',':
+		case ';':
+		case '.':
+		case '!':
+		case '?':
+		case '"':
+		case '(':
+		case ')':
+		case '{':
+		case '}':
+			if (s[i + 1] >= 97 && s[i + 1] <= 122 && s[i + 1] != '\0')
+				s[i + 1] -= 32;
+			if(s[i] == '\t')
+				s[i] = ' ';
+	}
+	return (s);
+}
