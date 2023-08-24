479a95fe881c88a3b0cf3546a77ee82f425a5ddc
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-23 22:20:56 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,11 +7,21 @@
  */
 char *rot13(char *s)
 {
-	int i;
+	int i, j;
+
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char n[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		s[i]
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = n[j];
+				break;
+			}
+		}
 	}
 	return (s);
 }
