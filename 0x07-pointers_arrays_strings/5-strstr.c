16cc4d4c9628568574adb164614428d0ddd2659f
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-28 06:26:21 -0700

    5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,18 +11,10 @@ char *_strstr(char *s, char *b)
 	unsigned int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{
 		if (s[i] == b[0])
-		{
-			for (j = 1; b[j] != '\0'; j++)
-			{
+			for (j = 0; b[j] != '\0'; j++)
 				if (s[i + j] == b[j])
-				{
 					if (b[j + 1] == '\0')
 						return (s + i);
-				}
-			}
-		}
-	}
 	return ('\0');
 }
