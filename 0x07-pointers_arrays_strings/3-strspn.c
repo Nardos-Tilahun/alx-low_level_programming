dcb3ecb6b7d8bdcd2c8ba78a96bc840e9a274f32
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-28 03:26:08 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,20 +8,22 @@
  */
 unsigned int _strspn(char *s, char *b)
 {
-	unsigned int i, j, c = 0, max = 0;
+	unsigned int i, j, c = 0;
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[j] != '\0'; j++)
+		for (j = 0; b[j] != '\0'; j++)
 		{
-			if (b[i] == s[j])
+			if (s[i] == b[j])
 			{
-				if (j >= max)
-					max = j;
 				c++;
 				break;
 			}
+			if (s[i] != b[j] && b[j + 1] == '\0')
+			{
+				return (c);
+			}
 		}
 	}
-	return (max + 1);
+	return (c);
 }
