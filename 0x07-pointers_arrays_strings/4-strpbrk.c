af28297593ef2e35fcc5d47d8f2e1268c9d51212
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-28 05:30:00 -0700

    4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - concatinate two string
+ * @s: first character pointer parameter
+ * @b: second character pointer parameter
+ * Return: pointer character
+ */
+char *_strpbrk(char *s, char *b)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; b[j] != '\0'; j++)
+		{
+			if (s[i] == b[j])
+			{
+				return (s + i);
+			}
+		}
+	}
+	return ("");
+}
