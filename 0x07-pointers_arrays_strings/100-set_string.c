291180f2b69cae2affcb7ca7ce64939ee271cda0
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-28 09:24:28 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - concatinate two string
+ * @s: first character pointer parameter
+ * @b: second character parameter
+ */
+void set_string(char **s, char *b)
+{
+	*s = b;
+}
