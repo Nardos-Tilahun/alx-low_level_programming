08757a45d0b541d056ab2c3bc8fdffdabdc3cedc
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-30 04:04:19 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * sqrt_other - square root
+ * @n: first integer
+ * @m: hold the integer number
+ * Return: integer
+ */
+
+int sqrt_other(int n, int m)
+{
+	if (n == 1)
+		return (1);
+	else if (n * n > m)
+		return (sqrt_other(n - 1, m));
+	else if (n * n == m)
+		return (n);
+	else	
+		return (-1);
+	return 1;
+}
+/**
+ * _sqrt_recursion - square root
+ * @n: first integer
+ * Return: integer
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrt_other(n, n));
+}
