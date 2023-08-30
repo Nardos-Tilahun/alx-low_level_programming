458279bbc2853ae774cf59dcd31c39cf6a86fd0f
Narrdos-Tilahun <contactnardos@gmail.com>
2023-08-30 05:07:46 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * is_prime_other - factorial
+ * @n: first integer
+ * @m: second integer
+ * Return: integer
+ */
+int is_prime_other(int n, int m)
+{
+	if (n == m && m != 2)
+		return (1);
+	else if (n % m == 0)
+		return (0);
+	return is_prime_other(n, ++m);
+}
+/**
+ * is_prime_number - factorial
+ * @n: first integer
+ * Return: integer
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return is_prime_other(n, 2);
+}
