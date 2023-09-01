531748590dc06044a3f781983dd10ad7ff03402f
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-01 08:48:58 -0700

    write all argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ * @argc: it counts argument
+ * @argv: it shows the arguments string
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", *(argv + i));
+	}
+	return (0);
+}
