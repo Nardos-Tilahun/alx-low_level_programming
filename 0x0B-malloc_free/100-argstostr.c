8ab0c5d1b0ae3cb4235b60db1a455af90bf920b8
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-04 22:48:18 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,10 +9,24 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i;
+	int i, j, s = 0, r = 0;
+	char *p;
 
-	for (i = 0; i < h; i++)
-		free(*(hp + i));
-	for (i = 0; i < h; i++)
-		free(hp + i);
+	for (i = 0; i < ac; i++)
+	{
+		s++;
+		for (j = 0; *(av[i] + j) != '\0'; j++)
+			s++;
+	}
+	p = (char *)malloc(sizeof(char) * ++s);
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; *(av[i] + j) != '\0'; j++, r++)
+			*(p + r) = *(av[i] + j);
+		*(p + r++) = '\n';
+	}
+	*(p + r) = '\0';
+	return (p);
 }
