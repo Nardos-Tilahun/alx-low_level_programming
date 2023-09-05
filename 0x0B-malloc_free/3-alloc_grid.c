b8eacc53d0d5e74518f60d2a576647d1276dfef2
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-04 20:28:13 -0700

    3-alloc_grid.c

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - create the array of specified number
+ * @w: first unsigned integer parameter
+ * @h: second character parameter
+ * Return: pointer integer
+ */
+int **alloc_grid(int w, int h)
+{
+	unsigned int i;
+	int **hp;
+
+	if (w == 0 || h == 0)
+		return (NULL);
+	hp = (int **)malloc(sizeof(int *) * h);
+	if (hp == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < h; i++)
+	{
+		*(hp + i) = (int *)malloc(sizeof(int) * w);
+		if ((hp + i) == NULL)
+		{
+			return (NULL);
+		}
+	}
+	return (hp);
+}
