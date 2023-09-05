3913588b197240b58dae59e937b5ad04d33ae093
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-04 20:53:29 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid -  newly allocated space in memory
+ * @hp: integer pointer to pointer
+ * @h: integer
+ */
+void free_grid(int **hp, int h)
+{
+	int i;
+
+	for (i = 0; i < h; i++)
+		free (*(hp + i));
+	for (i = 0; i < h; i++)
+		free (hp + i);
+}
