78110dabf8f7245bfe24298ffef805a195b46d38
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-11 05:22:45 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog- struct type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
