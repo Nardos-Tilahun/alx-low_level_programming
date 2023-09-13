6115b066645e969a6198d01a684aecf8e9a9b20f
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-13 02:24:09 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * int_index - create the array of specified number
+ * @array: integer pointer
+ * @size: size of the array
+ * @cmp: pointer function
+ * Return: integer
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (cmp == NULL || array == NULL)
+		exit(1);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+		if (cmp(array[i]))
+			return (i);
+	return (-1);
+}
