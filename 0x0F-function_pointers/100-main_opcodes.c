47dab3752e0903443dd739f81f6117379d0e816b
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-13 06:59:57 -0700

    100-main_opcodes.c

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,30 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+/**
+ * main - check the code
+ * @argc - integer
+ * @argv - pointer to pointer that point to character
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	char *a;
+	int i, len = atoi(argv[1]);
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	if (len < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	a = (char*)main;
+	for (i = 0; i < (len - 1); i++)
+		printf("%02hhx ", a[i]);
+	printf("%02hhx\n", a[i]);
+	return (0);
+}
