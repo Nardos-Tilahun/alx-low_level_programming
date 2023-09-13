7ed71b888e2bba11cf35ea72ffd68b1c0d5b5ebc
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-13 02:40:44 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,11 +11,11 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-
-	if (cmp == NULL || array == NULL)
-		exit(1);
+	
 	if (size <= 0)
 		return (-1);
+	if (cmp == NULL || array == NULL)
+		exit(1);
 	for (i = 0; i < size; i++)
 		if (cmp(array[i]))
 			return (i);
