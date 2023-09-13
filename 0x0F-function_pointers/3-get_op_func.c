73cac6712a0f07850debc69655dff0179f4d292b
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-13 05:54:53 -0700

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,9 +17,9 @@ int (*get_op_func(char *s))(int a, int b)
 		{NULL, NULL}
 	};
 
-	while (i < 6)
+	while (i < 5)
 	{
-		if (ops[i].op != NULL || *(ops[i].op) == *s)
+		if (*(ops[i].op) == *s)
 		{
 			return (ops[i].f);
 		}
