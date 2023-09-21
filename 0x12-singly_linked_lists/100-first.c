d6cc31ed7b2ac097661493ed215d1197670bfbe9
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-21 14:28:51 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-void _constructor() __attribute__((constructor));
+void _constructor(void) __attribute__((constructor));
 /**
  * _constructor - print the list in linked list
  */
-void _constructor()
+void _constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
