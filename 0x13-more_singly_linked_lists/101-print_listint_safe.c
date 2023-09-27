d27b8f766e43723b41bbd9975ff097cf5575d869
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-27 01:43:25 -0700

    101-print_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_listint_safe - print the list in linked list
+ * @h: - pointer to the header node
+ * Return: size of nodes
+ */
+size_t print_listint_safe(const listint_t *h)
+{
+	size_t countNode = 1;
+	const listint_t *one_time = h, *two_time = h;
+
+	if (h == NULL || h->next == NULL)
+		return (0);
+	one_time = one_time->next;
+	two_time = two_time->next->next;
+	while (two_time != NULL && two_time->next != NULL)
+	{
+		if (one_time == two_time)
+		{
+			one_time = h;
+			while (one_time != two_time)
+			{
+				countNode++;
+				printf("[%p] %d\n", (void *)one_time, one_time->n);
+				one_time = one_time->next;
+				two_time = two_time->next;
+			}
+			printf("[%p] %d\n", (void *)one_time, one_time->n);
+			one_time = one_time->next;
+			while (one_time != two_time)
+			{
+				countNode++;
+				printf("[%p] %d\n", (void *)one_time, one_time->n);
+				one_time = one_time->next;
+			}
+			printf("-> [%p] %d\n", (void *)one_time, one_time->n);
+			return (countNode);
+		}
+		one_time = one_time->next;
+		two_time = two_time->next->next;
+	}
+	while (h != NULL)
+	{
+		printf("[%p] %d\n", (void *)h, h->n);
+		h = h->next;
+		countNode != 1 ? countNode++ : countNode;
+	}
+	return (countNode);
+}
