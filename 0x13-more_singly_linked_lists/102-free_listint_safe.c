f89693fe0f864f52bca23ec9643f14e6cbeb70e3
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-27 05:31:08 -0700

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * help - print the list in linked list
+ * helper - print the list in linked list
  * @h: - pointer to the header node
  * Return: size of nodes
  */
-size_t help(const listint_t *h)
+size_t helper(listint_t **h)
 {
 	size_t countNode = 1;
-	const listint_t *one_time = h, *two_time = h;
+	listint_t *one_time = *h, *two_time = *h, *temp = *h;
 
-	if (h == NULL || h->next == NULL)
+	if (h == NULL || *h == NULL || (*h)->next == NULL)
 		return (0);
 	one_time = one_time->next;
 	two_time = two_time->next->next;
@@ -18,32 +18,28 @@ size_t help(const listint_t *h)
 	{
 		if (one_time == two_time)
 		{
-			one_time = h;
+			one_time = *h;
 			while (one_time != two_time)
 			{
 				countNode++;
-				printf("[%p] %d\n", (void *)one_time, one_time->n);
 				one_time = one_time->next;
 				two_time = two_time->next;
 			}
-			printf("[%p] %d\n", (void *)one_time, one_time->n);
 			one_time = one_time->next;
 			while (one_time != two_time)
 			{
 				countNode++;
-				printf("[%p] %d\n", (void *)one_time, one_time->n);
 				one_time = one_time->next;
 			}
-			printf("-> [%p] %d\n", (void *)one_time, one_time->n);
+			one_time->next = NULL;
 			return (countNode);
 		}
 		one_time = one_time->next;
 		two_time = two_time->next->next;
 	}
-	while (h != NULL)
+	while (temp != NULL)
 	{
-		printf("[%p] %d\n", (void *)h, h->n);
-		h = h->next;
+		temp = temp->next;
 		countNode != 1 ? countNode++ : countNode;
 	}
 	return (countNode);
@@ -56,11 +52,16 @@ size_t help(const listint_t *h)
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *temp;
+	size_t countNode = 0;
 
+	if (h == NULL || *h == NULL)
+		return (0);
+	countNode = helper(h);
 	while (*h != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = (*h)->next;
+		free(*h);
+		*h = temp;
 	}
+	return (countNode);
 }
