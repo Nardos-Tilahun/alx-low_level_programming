6740c7824f857a8d67f1459f63ce7a4d0b3aa4bf
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-29 12:10:27 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_bit - check the code
+ * @n: unisgned parameter
+ * @idx: index
+ * Return: Always integer sum.
+ */
+int get_bit(unsigned long int n, unsigned int idx)
+{
+	unsigned int i = 63;
+
+	while (i--)
+		if (((n >> i) & 1) == 1)
+			break;
+	if (i < idx)
+		return (-1);
+	else if ((n & (1 << idx)) != 0)
+		return (1);
+	return (0);
+}
