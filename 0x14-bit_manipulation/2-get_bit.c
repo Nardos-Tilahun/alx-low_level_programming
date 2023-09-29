99d7507f136bd64ace9e411f7c1bddd0ad69e6ff
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-29 14:11:59 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -18,7 +18,7 @@ int get_bit(unsigned long int n, unsigned int idx)
 	}
 	if (i < idx || idx > 63)
 		return (-1);
-	return (((n >> idx) & 1))
+	return (((n >> idx) & 1));
 		return (1);
 	return (0);
 }
