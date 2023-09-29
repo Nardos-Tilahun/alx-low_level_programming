41033d86effd220edacf11bd6b417425dabc4a1c
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-29 12:22:16 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,9 +10,12 @@ int get_bit(unsigned long int n, unsigned int idx)
 {
 	unsigned int i = 63;
 
-	while (i--)
+	while (i)
+	{
+		i--;
 		if (((n >> i) & 1) == 1)
 			break;
+	}
 	if (i < idx)
 		return (-1);
 	else if ((n & (1 << idx)) != 0)
