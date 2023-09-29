60bb1d21a141a84a004618966e623df9e08c00b7
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-29 13:48:39 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - check the code
+ * @n: unisgned parameter
+ * @m: index
+ * Return: Always integer sum.
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i = 64, count = 0;
+	unsigned long int k = 0;
+
+	if (n == 0 && m == 0)
+		return (k);
+	else if (n == 0)
+		k = (~m);
+	else if (m == 0)
+		k = (~n);
+	else
+		k = n ^ m;
+	while (--i >= 0)
+		if (((k >> i) & 1) == 1)
+			count++;
+	return (count);
+}
