64fc0158155ce0567550b03289786b31001470a7
Narrdos-Tilahun <contactnardos@gmail.com>
2023-09-29 13:01:02 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * set_bit - check the code
+ * @n: unisgned parameter
+ * @idx: index
+ * Return: Always integer sum.
+ */
+int set_bit(unsigned long int *n, unsigned int idx)
+{
+	unsigned int i = 63;
+
+	while (i)
+	{
+		i--;
+		if (((*n >> i) & 1) == 1)
+			break;
+	}
+	if (i < idx && *n != 0)
+		return (-1);
+	*n = (*n | (1 << idx));
+	return (1);
+}
