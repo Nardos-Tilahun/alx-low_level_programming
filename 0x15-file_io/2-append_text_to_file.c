b06f23e82caeed41aeecf11a2fd5109980dc290a
Nardos <contactnardos@gmail.com>
2023-10-02 20:50:36 +0000

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,29 +2,29 @@
 #include <stdlib.h>
 
 /**
- * append_text_to_file - check the code
+ * appen_text_to_file - check the code
  * @filename: file name
  * @text_content: letter
  * Return: Always 0.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	ssize_t n;
-	ssize_t m, p;
-	char *buff;
+	int i = 0;
+	int p, q;
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
-	p = open(filename, O_APPEND);
+	if (text_content != NULL)
+		while (text_content[i] != '\0')
+			i++;
+	p = open(filename, O_CREAT | O_RDWR | O_APPEND);
 	if (p == -1)
-	{
-		return (0);
-	}
-	n = read(p, buff, letters);
-	m = write(1, buff, n);
-	free(buff);
+		return (-1);
+	q = write(p, text_content, i);
+	if (q == -1)
+		return (-1);
 	close(p);
-	return (m);
+	return (1);
 }
