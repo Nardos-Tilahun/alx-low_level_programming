b3f9224e02340125cd9da859820ccb858138e06d
Nardos <contactnardos@gmail.com>
2023-12-06 04:59:19 +0000

    100-sorted_hash_table.c

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - gives the index of the key
+ * @key: the key which is given
+ * @size: the size of the array
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	
+		
+}
